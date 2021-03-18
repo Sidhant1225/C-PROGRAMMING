bfa707208a8c3c132e75cbfbedc8f66a58607093
Sidhant1225 <80540426+Sidhant1225@users.noreply.github.com>
2021-03-18 11:31:05 +0530

    Add files via upload

---
+++ No. divisible by 5 or 11.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+int n;
+printf("enter number n");
+scanf("%d",&n);
+if(n%5==0){
+printf("n is divisible by 5");
+}
+else if(n%11==0){
+printf("n is divisible by 11");
+}
+else{
+printf("n is not divisible by 5 and 11");
+}}
