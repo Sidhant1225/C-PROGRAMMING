0d23cbbf70b78c914845a7c33c6a6b0dfec7349b
Sidhant1225 <80540426+Sidhant1225@users.noreply.github.com>
2021-03-23 00:18:52 +0530

    Add files via upload

---
+++ Number is negative,positive or zero.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+int n;
+printf("enter number n");
+scanf("%d",&n);
+if(n>0){
+printf("n is positive");
+}
+else if(n<0){
+printf("n is negative");
+}
+else{
+printf("n is zero");
+}}
