b273784527dbb72c33d02a3b4816e2960eae7dd7
Sidhant1225 <80540426+Sidhant1225@users.noreply.github.com>
2021-03-26 13:56:53 +0530

    Add files via upload

---
+++ tablefunction.c
@@ -0,0 +1,11 @@
+//wpa to define a function which should print a table of given no.
+int table(int n)
+{int i;
+    for(i=1;i<=10;i++)
+    {
+        printf("%d \n",n*i);
+    }
+}
+void main(){
+table(5);
+}
