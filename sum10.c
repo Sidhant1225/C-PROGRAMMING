70d21d2e4b4a36431a8b4125e46fca5a0168ecd4
Sidhant1225 <80540426+Sidhant1225@users.noreply.github.com>
2021-03-26 13:56:31 +0530

    Add files via upload

---
+++ sum10.c
@@ -0,0 +1,13 @@
+// wap to define  function which should return sum of first 10 natural no.
+int sum()
+{int i,r=0;
+    for(i=1;i<=10;i++)
+    {
+        r=r+i;
+    }
+    return(r);
+
+}
+void main(){
+printf("%d",sum());
+}
