5f6d1442aa90cff4056c4f039a0a4a0038dc6de5
Sidhant1225 <80540426+Sidhant1225@users.noreply.github.com>
2021-03-30 14:21:17 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+int a,b;
+printf("enter a\n");
+printf("enter b");
+scanf("%d%d",&a,&b);
+printf("mult = %d \n",a*b);
+printf("sub = %d \n ",a-b);
+printf("add = %d \n",a+b);
+printf("div = %d \n",a/b);
+}
+
