4648ef2b2188d3d904c105df66c312bf66d9def8
Sidhant1225 <80540426+Sidhant1225@users.noreply.github.com>
2021-04-06 00:05:01 +0530

    Add files via upload

---
+++ cm to m and km.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+main()
+{
+float a;
+printf("enter a=");
+scanf("%f",&a);
+printf("centimeter to meters=");
+printf("%f",a/100);
+printf("\ncentimeter to kilometer=");
+printf("%f",a/100000);
+}
+
+
+
+
