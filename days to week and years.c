85164f00d2887523bccd10731beeb82ac1fc9a5c
Sidhant1225 <80540426+Sidhant1225@users.noreply.github.com>
2021-04-06 00:05:18 +0530

    Add files via upload

---
+++ days to week and years.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+main()
+{
+float d,w,y;
+printf("enter the number of days");
+scanf("%f",&d);
+w=d/7;
+printf("number of weeks are %f",w);
+y=d/365;
+printf("number of years are %f",y);
+return 0;
+}
