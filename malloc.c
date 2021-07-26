4d975169f07c32479534cc47f55c34a648993886
Sidhant1225 <80540426+Sidhant1225@users.noreply.github.com>
2021-07-26 21:25:47 +0530

    Add files via upload

---
+++ malloc.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+//#include<malloc.h>
+//int main()
+//{
+//int *a,n,i,x=0;
+//scanf("%d",&n);
+//a=(int *)malloc(n*sizeof(int));
+//for(i=0;i<n;i++)
+//{
+//scanf("%d",&a);
+//}
+//char *b;
+//b=(char *)malloc(n*sizeof(char));
+//free(a);
+//int *b=a;
+//printf("%d",b);
+
+//}
